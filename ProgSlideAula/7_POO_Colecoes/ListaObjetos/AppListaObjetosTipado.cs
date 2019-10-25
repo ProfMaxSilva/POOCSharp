a139a5488937e668f0d4b2f2689d4d896a6ccbcb
Prof º Max Silva <profmaxsilva@gmail.com>
2019-10-25 05:07:46 -0300

    adição de comentarios nas coleções

--- ProgSlideAula/7_POO_Colecoes/ListaObjetos/AppListaObjetosTipado.cs
+++ ProgSlideAula/7_POO_Colecoes/ListaObjetos/AppListaObjetosTipado.cs
@@ -5,24 +5,37 @@ namespace ProgSlideAulas.Colecoes.ListaObjetos
 {
     public class AppListaObjetosTipado
     {
-        public void ExecutaExemplo()
-        {
-            List<Cliente> Clientes = new List<Cliente>();
-            Cliente c1 = new Cliente() { Codigo = 1, Nome = "Max", Saldo = 20.0 }; // mostrar pro aluno como fazer passando todos os parametros de uma só vez
-            Cliente c2 = new Cliente { Codigo = 2, Nome = "Max2", Saldo = 20.0 };
-            Cliente c3 = new Cliente(03);
+        //public static int Main(string[] args)
+        //{
+        //    /* 
+        //     * A maioria das coleções implementam as mesmas interfaces e estas interfaces podem ser herdadas para criar classes de coleções mais especializadas.
+        //     * Como funciona o armazenamento de elementos em uma coleção?
+        //     * Uma coleção armazena elementos como Object, assim se eu guardar um item do tipo String em uma coleção ela irá armazenar um object.Se for um integer a mesma coisa e assim por diante.
+        //     * Com isto temos sempre que estar tipando manualmente cada item da coleção quando formos tratá - los.Desta forma quando eu coloco um item em uma coleção faço o boxing e quando eu retiro um item da coleção faço o unboxing.
+        //     * Obs: Boxing: É a conversão de um Value Type para um Reference Type e Unboxing: É quando um Reference Type(object) volta a ser um Value Type.
+        //        ESSE TEXTO FOI TIRADO DO SITE DO macoratti NA PAGINA http://www.macoratti.net/12/06/c_colec1.htm
+        //     */
+        //    List<Cliente> Clientes = new List<Cliente>();
+        //    // mostrar pro aluno como fazer passando todos os parametros de uma só vez
+        //    Cliente c1 = new Cliente() { Codigo = 1, Nome = "Max", Saldo = 20.0 };
+        //    Cliente c2 = new Cliente { Codigo = 2, Nome = "Max2", Saldo = 20.0 };
+        //    Cliente c3 = new Cliente(03);
 
-            Clientes.Add(c1);
-            Clientes.Add(c2);
-            Clientes.Add(c3);
-            Clientes.Add(new Cliente(4));// mostrar pro aluno que dar pra passar instancia
+        //    Clientes.Add(c1);
+        //    Clientes.Add(c2);
+        //    Clientes.Add(c3);
+        //    Clientes.Add(new Cliente(4));// mostrar pro aluno que dar pra passar instancia
 
-            for (int i = 0; i < Clientes.Count; i++)
-            {
-                Cliente aux = (Cliente)Clientes[i];
+        //    foreach (Cliente aux in Clientes)// usando o for avançado
+        //    //for (int i = 0; i < Clientes.Count; i++)
+        //    {
+        //        // não precisa mais fazer o cast, a lista é tipada
+        //        //Cliente aux = Clientes[i];
 
-                Console.WriteLine(aux.ToString());
-            }
-        }
+        //        Console.WriteLine(aux.ToString());
+
+        //    }
+
+        //}
     }
 }
